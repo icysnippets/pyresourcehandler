65893d982c8d209db5ea14868bb9b249c5031676
user <user@example.org>
2022-09-12 00:34:34 +1000

    Added tests to get 100% coverage

---
+++ src/ResourcesHandler/ResourcesHandler.py
@@ -0,0 +1,218 @@
+import importlib.resources
+import shutil
+from importlib.abc import Traversable
+from pathlib import Path
+
+def _loadModule(modulePath: str) -> Traversable:
+
+    """
+    Gets a Traversable resource from the given module path.
+
+    :param modulePath: Path to the module.
+    :return: Returns a Traversable object.
+    """
+
+    return importlib.resources.files(modulePath)
+
+def _getResourcesRoot(modulePath: str) -> Traversable:
+
+    """
+    Gets the resources root of a given module.
+    Module should contain a directory in its root with one of the following names:
+    -resources
+    -data
+    -assets
+
+    :param modulePath: Path to the module.
+    :return: Returns Traversable object to the resource root.
+    """
+    #Get the module
+    module = _loadModule(modulePath)
+
+    #List of possible names for the resource root
+    resourceRootNames = ["resources", "data", "assets"]
+
+    #Iterate through the list of possible resource root names
+    for resourceRootName in resourceRootNames:
+
+        # Attempt to find the resource root
+        resourceRoot = module \
+            .joinpath(resourceRootName)
+
+        #If the resource root was successfully found
+        if resourceRoot.is_dir():
+
+            #Return the resources root
+            return resourceRoot
+
+    #Create the error message
+    errorMsg = f"No resources directory found in module {modulePath}\n"
+    errorMsg += "Resource directory should be named one of the following:\n"
+    for resourceRootName in resourceRootNames: errorMsg += f"-{resourceRootName}\n"
+    raise FileNotFoundError(errorMsg)
+
+def extractFile(
+    modulePath: str,
+    resourceFile: str,
+    outputPath: Path,
+    overwrite: bool = False
+):
+
+    """
+    Extracts a resource file embedded within the application.
+
+    :param modulePath: Path to import module
+    :param resourceFile: Path expression to the resource file.
+    :param outputPath: Path to the output file.
+    :param overwrite: Whether the output file should be overwritten if it already exists.
+    """
+
+    #Load the resources root
+    module = _getResourcesRoot(modulePath)
+
+    #Get the resource at the given path
+    resource = module \
+        .joinpath(resourceFile)
+
+    #Determine whether a file is found at the given path
+    fileFound = resource.is_file()
+
+    #If no file was found matching the given expression
+    if not fileFound:
+
+        #Create an error message
+        errorMsg = f"Error: No file found matching expression '{resourceFile}'"
+
+        #Throw an error message here
+        raise FileNotFoundError(errorMsg)
+
+    #Ensure the output path's parent directory exists
+    outputPath.parent.mkdir(
+        exist_ok=True,
+        parents=True
+    )
+
+    #If the output file should not be overwritten
+    if overwrite == False and outputPath.exists():
+
+        #Create the error message
+        errorMsg = f"Error: '{outputPath}' already exists\n"
+        errorMsg += "Cannot overwrite existing file"
+
+        #Throw the error
+        raise FileExistsError(errorMsg)
+
+    #Write the contents to the file
+    with open(outputPath, "wb") as outputFile:
+
+        #Extract the content of the embedded resource file
+        outputFile.write(resource.read_bytes())
+
+def _iterateDir(resource: Traversable) -> list[Traversable]:
+
+    """
+    Gets a recursive list of all files(including directories).
+
+    :param resource: Expected to be a directory.
+    :return: Returns a list of traversable objects.
+    """
+
+    #Create a list to store the resources including the current resource
+    resources = []
+
+    #Iterate through the resources in this directory
+    for f in resource.iterdir():
+
+        #If the current file is a directory
+        if f.is_dir():
+
+            #Add this directory to the list of resources
+            resources.append(f)
+
+            #Add the list of resources within this subdirectory
+            resources.extend(_iterateDir(f))
+
+        #If this is a file
+        else:
+
+            resources.append(f)
+
+    #Return the list of resources
+    return resources
+
+def extractDir(
+        modulePath: str,
+        resourceFile: str,
+        outputDir: Path,
+        overwrite: bool = False
+):
+    """
+    :param modulePath: Path to import module
+    :param resourceFile: Path expression to the resource directory.
+    :param outputDir: Path to the output directory.
+    :param overwrite: Whether the output directory should be overwritten if it already exists.
+    """
+
+    # Load the resources root
+    module = _getResourcesRoot(modulePath)
+
+    #Get the resource at the given path
+    resource = module \
+        .joinpath(resourceFile)
+
+    #Determine whether a directory is found at the given path
+    directoryFound = resource.is_dir()
+
+    #If no directory was found matching the given expression
+    if not directoryFound:
+
+        #Create an error message
+        errorMsg = f"Error: No directory found matching expression '{resourceFile}'"
+
+        #Throw an error message here
+        raise FileNotFoundError(errorMsg)
+
+    #If the output directory should not be overwritten
+    if overwrite == False and outputDir.exists():
+
+        #Create the error message
+        errorMsg = f"Error: '{outputDir}' already exists\n"
+        errorMsg += "Cannot overwrite existing directory"
+
+        #Throw the error
+        raise FileExistsError(errorMsg)
+
+    #Delete the existing directory(if one exists)
+    if outputDir.exists(): shutil.rmtree(outputDir)
+
+    #Ensure the output directory exists
+    outputDir.mkdir(parents=True)
+
+    # Get a recursive list of files in the directory
+    filesRecursiveList = _iterateDir(resource)
+
+    # Iterate through the list of files
+    for f in filesRecursiveList:
+
+        # Get the relative path for this resource
+        relativePath = str(f).replace(str(resource), "")
+
+        # Create the output path for this resource
+        outputPath = Path(
+            f"{outputDir}/{relativePath}"
+        )
+
+        # If the resource is a directory
+        if f.is_dir():
+
+            # Ensure the directory is created
+            outputPath.mkdir(exist_ok=True, parents=True)
+
+        # Else if the resource is a file
+        else:
+
+            # Write the contents to the file
+            with open(outputPath, "wb") as outputFile:
+
+                # Extract the content of the embedded resource file
+                outputFile.write(f.read_bytes())
---
+++ tests/TestResourcesHandler.py
@@ -0,0 +1,269 @@
+import os
+import tempfile
+from pathlib import Path
+import pytest
+
+from src.ResourcesHandler.ResourcesHandler import extractFile, extractDir
+
+#Path to the embedded file
+_resourceFile = "file_example.txt"
+
+#Path to the invalid ".py" file
+_resourceInvalidPyFile = "invalid_python_example.py"
+
+#Path to the embedded directory
+_resourceDir = "dir_example"
+
+class TestResourcesHandler:
+    def testGetResourcesRootFailure(self):
+
+        """
+        Attempts to extract a file from a module with a data folder.
+        Should fail since module does not use a predefined data-folder name.
+        Expects failure.
+        """
+
+        try:
+
+            with tempfile.TemporaryFile() as tmpFile:
+
+                #Attempt to extract a file
+                extractFile(
+                    "tests.Example_Module_Bad", #Intentionally incorrect
+                    _resourceFile,
+                    tmpFile
+                )
+
+            # Create the error message
+            errorMsg = f"'{extractFile.__name__}' should not have found the module"
+            pytest.fail(errorMsg)
+
+        except FileNotFoundError:
+
+            #Do nothing since this is expected behaviour
+            pass
+
+    def testExtractFile(self):
+
+        """
+        Makes sure that regular files are extracted correctly.
+        """
+
+        #Iterate twice to test the 'overwrite' argument
+        for i in range(2):
+
+            with tempfile.TemporaryDirectory() as tmpDir:
+
+                #Output path for the resource file
+                outputPath = Path(tmpDir, _resourceFile)
+
+                #Make sure the directory has 0 files
+                assert 0 == len(os.listdir(tmpDir))
+
+                #Create the file for the second iteration
+                if i == 1: outputPath.write_text("")
+
+                extractFile(
+                    "tests.Example_Module",
+                    _resourceFile,
+                    outputPath,
+                    overwrite=i == 1  # Overwrite the file created in the 2nd iteration
+                )
+
+                #Make sure the directory has exactly 1 file
+                assert 1 == len(os.listdir(tmpDir))
+
+                #Read the contents of the extracted resource file
+                contents = outputPath.read_text(encoding="utf-8")
+
+                #Make sure the file contains the correct content
+                assert contents == "123"
+
+    def testExtractFileFail(self) -> None:
+
+        """
+        Attempts to extract a directory using the method for files.
+        Expects failure.
+        """
+
+        try:
+
+            with tempfile.TemporaryFile() as tmpFile:
+
+                #Attempt to extract a directory using the method for files
+                extractFile(
+                    "tests.Example_Module",
+                    _resourceDir, #Intentionally incorrect
+                    tmpFile
+                )
+
+            # Create the error message
+            errorMsg = f"'{extractFile.__name__}' should fail to extract/find directories"
+            pytest.fail(errorMsg)
+
+        except FileNotFoundError:
+
+            #Do nothing since this is expected behaviour
+            pass
+
+    def testExtractFileFail2(self) -> None:
+
+        """
+        Attempts to extract a file to an existing file without specifying overwrite.
+        Expects failure.
+        """
+
+        try:
+
+            with tempfile.TemporaryDirectory() as tmpDir:
+
+                # Output path for the resource file
+                outputPath = Path(tmpDir, _resourceFile)
+
+                #Ensure the file exists
+                outputPath.write_text("")
+
+                #Attempt to extract a file
+                extractFile(
+                    "tests.Example_Module",
+                    _resourceFile,
+                    outputPath,
+                    overwrite=False #Intentionally incorrect
+                )
+
+            # Create the error message
+            errorMsg = f"'{extractFile.__name__}' should fail to overwrite files without 'overwrite' option"
+            pytest.fail(errorMsg)
+
+        except FileExistsError:
+
+            #Do nothing since this is expected behaviour
+            pass
+
+    def testExtractInvalidPyFile(self) -> None:
+
+        """
+        Makes sure that invalid python files packaged as resources are extracted correctly.
+        """
+        with tempfile.TemporaryDirectory() as tmpDir:
+
+            #Output path for the resource file
+            outputPath = Path(tmpDir, _resourceInvalidPyFile)
+
+            #Make sure the directory has 0 files
+            assert 0 == len(os.listdir(tmpDir))
+
+            extractFile(
+                "tests.Example_Module",
+                _resourceInvalidPyFile,
+                outputPath
+            )
+
+            #Make sure the directory has exactly 1 file
+            assert 1 == len(os.listdir(tmpDir))
+
+            #Read the contents of the extracted resource file
+            contents = outputPath.read_text(encoding="utf-8")
+
+            #Make sure the file contains the correct content
+            assert contents == "print(hello world)"
+
+    def testExtractDir(self) -> None:
+
+        """
+        Makes sure that invalid python files packaged as resources are extracted correctly.
+        """
+
+        #Iterate twice to test the 'overwrite' argument
+        for i in range(2):
+
+            with tempfile.TemporaryDirectory() as tmpDir:
+
+                #Create the directory for the second iteration
+                if i == 1: Path(tmpDir).mkdir(exist_ok=True, parents=True)
+
+                #Output path for the resource file
+                outputPath = Path(tmpDir, _resourceDir)
+
+                #Make sure the directory has 0 files
+                assert 0 == len(os.listdir(tmpDir))
+                assert False == outputPath.is_dir()
+
+                extractDir(
+                    "tests.Example_Module",
+                    _resourceDir,
+                    outputPath,
+                    overwrite = i == 1 #Overwrite the directory created in the 2nd iteration
+                )
+
+                #Make sure the directory has exactly 1 directory
+                assert 1 == len(os.listdir(tmpDir))
+                assert True == outputPath.is_dir()
+
+                #Create the path to the expected files
+                helloWorld1 = outputPath.joinpath("hello world 1.txt")
+                helloWorld2 = outputPath.joinpath("nested_dir", "hello world 2.txt")
+
+                #Make sure the expected files hold the correct content
+                assert "hello world 1" == helloWorld1.read_text()
+                assert "hello world 2" == helloWorld2.read_text()
+
+    def testExtractDirFail(self) -> None:
+        """
+        Attempts to extract a file using the method for directories.
+        Expects failure.
+        """
+
+        try:
+
+            with tempfile.TemporaryDirectory() as tmpDir:
+
+                #Attempt to extract a file using the method for directories
+                extractDir(
+                    "tests.Example_Module",
+                    _resourceFile, #Intentionally incorrect
+                    tmpDir
+                )
+
+            # Create the error message
+            errorMsg = f"'{extractDir.__name__}' should fail to extract/find files"
+            pytest.fail(errorMsg)
+
+        except FileNotFoundError:
+
+            #Do nothing since this is expected behaviour
+            pass
+
+    def testExtractDirFail2(self) -> None:
+
+        """
+        Attempts to extract a directory to an existing file without specifying overwrite.
+        Expects failure.
+        """
+        try:
+
+            with tempfile.TemporaryDirectory() as tmpDir:
+
+                # Output path for the resource directory
+                outputPath = Path(tmpDir, _resourceDir)
+
+                #Ensure the directory exists
+                outputPath.mkdir(parents=True)
+
+                #Attempt to extract a directory
+                extractDir(
+                    "tests.Example_Module",
+                    _resourceDir,
+                    outputPath,
+                    overwrite=False #Intentionally incorrect
+                )
+
+            # Create the error message
+            errorMsg = f"'{extractFile.__name__}' should fail to overwrite directories without 'overwrite' option"
+            pytest.fail(errorMsg)
+
+        except FileExistsError:
+
+            #Do nothing since this is expected behaviour
+            pass
+
